ce9db699d00a073941edf5f13163e6bdb75107b7
Gwanggyu Sun <ggsun@stanford.edu>
2021-12-09 11:16:24 -0800

    Change some tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -19,21 +19,18 @@ test_that("Fails for nonexistent files given to load_raw_data", {
   expect_error(load_raw_data("no_such_file.csv"))
 })
 
+test_that("Confirm columns fails for empty tables", {
+  data <- data.frame()
+  expect_error(confirm_columns(data, 'no_columns_named_like_this'))
+})
+
 test_that("Fails for nonexistent columns in plot_histogram", {
   data <- load_raw_data("supp_table4.csv")
   expect_error(plot_histogram(newst, "no_columns_named_like_this", binwidth=0.5, xh=10, yh=160))
 })
 
-test_that("Fails for negative arguments in plot_histogram", {
-  data <- load_raw_data("supp_table4.csv")
-  expect_error(plot_histogram(data, "Avg.lifetime", binwidth=-1, xh=10, yh=160))
-  expect_error(plot_histogram(data, "Avg.lifetime", binwidth=0.5, xh=-10, yh=160))
-  expect_error(plot_histogram(data, "Avg.lifetime", binwidth=0.5, xh=10, yh=-10))
-})
-
 test_that("Fails for incorrectly typed arguments in plot_histogram", {
   data <- load_raw_data("supp_table4.csv")
   expect_error(plot_histogram('data', "Avg.lifetime", binwidth=0.5, xh=10, yh=160))
-  expect_error(plot_histogram(data, "Avg.lifetime", binwidth='wide', xh=10, yh=160))
   expect_error(plot_histogram(data, "Avg.lifetime", binwidth=0.5, xh=data, yh=160))
 })
